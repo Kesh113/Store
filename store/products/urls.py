78b56083cb3e148df91aebe906a235e0a3809b3f
Kesh113 <kesh193@yandex.ru>
2024-08-07 17:11:21 +0700

    I finished the design of the header and footer, stopped at the design of the catalog on the main page of the site. The backend is partially connected.

--- store/products/urls.py
+++ store/products/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.products_home, name='home'),
+    path('cart/', views.products_cart, name='cart'),
+    path('login/', views.products_login, name='login'),
+    path('about/', views.products_about, name='about'),
 ]
--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def products_home(request):
-    return render(request, 'products/index.html')
+    return render(request, 'products/index.html', {'title': 'Магазин товаров'})
+
+def products_cart(request):
+    return render(request, 'products/cart.html')
+
+def products_login(request):
+    return render(request, 'products/login.html')
+
+def products_about(request):
+    return render(request, 'products/about.html')
